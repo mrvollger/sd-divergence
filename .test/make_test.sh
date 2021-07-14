0fc17077db899333c1ead48084b75f08930cfb8d
Mitchell Robert Vollger <mrvollger@gmail.com>
2021-07-14 11:28:43 -0700

    test

--- .test/make_test.sh
+++ .test/make_test.sh
@@ -11,9 +11,10 @@ ls -d /net/eichler/vol27/projects/hprc/nobackups/variants/pav/202103/pav_mm2_chm
   && cp {}/callable/callable_regions_h1_500.bed.gz data/{/.}/. \
   && cp {}/align/pre-cut/aligned_tig_h2.bed.gz data/{/.}/. \
   && cp {}/callable/callable_regions_h2_500.bed.gz data/{/.}/. \
-  && zcat {}/bed/snv_snv.bed.gz | head -n 1000 | gzip > data/{/.}/snv_snv.bed.gz
+  && zcat {}/bed/snv_snv.bed.gz | head -n 100000 | gzip > data/{/.}/snv_snv.bed.gz
 '
 
+exit
 
 printf "sample\th1_aln\th2_aln\th1_callable\th2_callable\tsnv\n"
 
