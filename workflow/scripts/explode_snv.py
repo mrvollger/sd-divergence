4f59828c694e68e78d8bcba7080061a30524a12f
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-02-21 12:08:02 -0800

    new setup

--- workflow/scripts/explode_snv.py
+++ workflow/scripts/explode_snv.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
 df = pd.read_csv(snakemake.input.bed, sep="\t", low_memory=False)
-print(f"All things in the vcf: {df.shape}")
+print(f"All things in the vcf: {df.shape[0]/1e6}")
 df = df[((df["TYPE"] == "SNV") | (df["TYPE"] == "SNP")) & (df["#CHROM"] != "chrY")]
-print(f"SNP and not chrY: {df.shape}")
+print(f"SNP and not chrY: {df.shape[0]/1e6}")
 
 df = df.astype(str)
 df = (
@@ -16,5 +16,5 @@ df.loc[(df["HAP"] == "h1") & (df["GT"] == "1|1"), "GT"] = "1|."
 df.loc[(df["HAP"] == "h2") & (df["GT"] == "1|1"), "GT"] = ".|1"
 
 df["SAMPLE"] = snakemake.wildcards.sm
-print(f"SNP and not chrY exploded: {df.shape}")
+print(f"SNP and not chrY exploded: {df.shape[0]/1e6}")
 df.to_csv(snakemake.output.snv, sep="\t", compression="gzip", index=False)
